f62ff0f5576983a8d5c389106cc9538fcdeeca58
LMK1985 <lammankid@gmail.com>
2021-03-10 01:39:58 +0800

    Added fadebox

--- js/script.js
+++ js/script.js
@@ -1,13 +1,25 @@
 $(function(){  //Initialize
     
-    $("#hide").click(function(){
-        $("p").hide();
-        });
-        $("#show").click(function(){
-        $("p").show();
-        });
-        $("#toggle").click(function(){
-        $("p").toggle();
+    // Hide Show Toggle Para
+    $("#hide").click(function() {
+        $("p").hide(1000);          // Demo .hide()
+    });
+    $("#show").click(function() {
+        $("p").show(1000);           // Demo .show()
+    });
+    $("#toggle").click(function() {
+        $("p").toggle(1000);        // Demo .toggle()
     });
 
+    // Fade In Out Toggle Div
+    $("#fadeboxin").click(function() {
+        $("#div1").fadeIn();
+        $("#div2").fadeIn("slow");
+        $("#div3").fadeIn(3000);
+    });
+    $("#fadeboxout").click(function() {
+        $("#div1").fadeOut();
+        $("#div2").fadeOut("slow");
+        $("#div3").fadeOut(3000);
+    });
 });
