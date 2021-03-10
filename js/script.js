eee73af17bdbb3e273eabafe71bdeed0763c0b5f
LMK1985 <lammankid@gmail.com>
2021-03-11 02:58:59 +0800

    Completed page 12 of lecture 7

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ $(function(){  //Initialize
         $("p").toggle(1000);        // Demo .toggle()
     });
 
-    // Fade In Out Toggle Div
+    // Fade In Out Toggle To Opacity Div
     $("#fadeboxin").click(function() {
         $("#div1").fadeIn();
         $("#div2").fadeIn("slow");
@@ -21,5 +21,37 @@ $(function(){  //Initialize
         $("#div1").fadeOut();
         $("#div2").fadeOut("slow");
         $("#div3").fadeOut(3000);
+    });
+    $("#togglebox").click(function() {
+        $("#div1").fadeToggle();
+        $("#div2").fadeToggle("slow");
+        $("#div3").fadeToggle(3000);
+    });
+    $("#fadeto").click(function() {       // Can customize transparency, does not collapse HTML upon transparent
+        $("#div1").fadeTo("slow", 0);
+        $("#div2").fadeTo("slow", 0.5);
+        $("#div3").fadeTo("slow", 0.7);
+    });
+    $("#flip").click(function() { 
+        $("#panel").slideToggle("slow"); // Can also be slideDown() or slideUp() for single function
+    });
+    $("#animation").click(function() {
+        $("#animate").animate({left: '250px'});
+    });
+    $("#animationmulti").click(function() {
+        $("#animate").animate({
+            left: '250px',
+            top: '900px',
+            opacity: '0.5',
+            height: '150px',
+            width: '300px'
+        });
+    });
+    $("#relative").click(function() {
+        $("#relativeanimate").animate({
+            left: '+=50px',   // This way of css adds to the position
+            height: '+=150px',
+            width: '+=50px'
+        });
     });
 });
